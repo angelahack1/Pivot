9cef1c81409c3d448e8fe839a740277a671d9a3e
angelahack1 <angelakimichellle@hotmail.com>
2023-11-13 18:35:53 -0600

    by angelahack1

--- index.mjs
+++ index.mjs
@@ -1,15 +1,14 @@
 import express from 'express';
-import path from 'path';
-import { fileURLToPath } from 'url';
 import { MongoClient } from 'mongodb';
 import moment from 'moment';
 import bodyParser from 'body-parser';
+import { readFile } from 'node:fs';
 
 const testing = process.env.TESTING;
 const log_level = process.env.LEVEL;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var port = 5555;
 
@@ -19,48 +18,70 @@ if(testing === 1) {
     port = 80;
 }
 
-async function run(dateArg, timeArg, emailArg, commentArg, ipArg) {
- const uri = "mongodb://angelalm:1164Louder@127.0.0.1:27017/artecnologia_db?retryWrites=true&w=majority";
- const client = new MongoClient(uri);
+async function sendToMongo(uri, dateArg, timeArg, emailArg, commentArg, ipArg) {
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        const collection = client.db("artecnologia_db").collection("req_for_comments");
+        const document = { date: dateArg, time: timeArg, email: emailArg, comment: commentArg, ip: ipArg }
+        console.log('About to insert to db this document: ', document); 
+        const result = await collection.insertOne(document);
+        console.log('Document inserted successfully:', result);
+    } catch (error) {
+        console.log('Error inserting document:', error);
+    } finally {
+        await client.close();
+    }
 
- try {
-    await client.connect();
-    const collection = client.db("artecnologia_db").collection("req_for_comments");
-    const document = { date: dateArg, time: timeArg, email: emailArg, comment: commentArg, ip: ipArg }
-    console.log('About to insert to db this document: ', document); 
-    const result = await collection.insertOne(document);
-    console.log('Document inserted successfully:', result);
- } catch (error) {
-    console.log('Error inserting document:', error);
- } finally {
-    await client.close();
- }
 }
 
-app.use(express.static(path.join(__dirname, './')));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+async function run(dateArg, timeArg, emailArg, commentArg, ipArg) {
+    var uri = null;
+    console.log('About to read from credentials...');
+    readFile('./credentials.dat', {encoding: 'utf8'}, (err, data) => {
+        if (err) {
+            console.log('Cannot read credentials to connect to mongodb', err);
+            return;
+        } else {
+            uri = data;
+            console.log('URI loaded to connecto to mongodb: ', uri);
+            sendToMongo(uri, dateArg, timeArg, emailArg, commentArg, ipArg).catch(console.log);
+        }
+    });
+}
 
 app.post('/contact_form', (req, res) => {
     const email = req.body.email;
     const comment = req.body.comment;
-    var ip = req.ip;
-    console.log('Req received from:',ip,' posting: ', req.body);
-    var currentDate = moment().format('YYYY/MM/DD');
-    var currentTime = moment().format('HH:mm:ss');
-    run(currentDate, currentTime, email, comment, ip).catch(console.log);
-    res.redirect('http://aixkare.com/thanks.html');
+    console.log('Req for comment received with: Email: <', email, '>, Comment: <', comment,'>');
+    if (typeof email === 'undefined' || email === null || typeof comment === 'undefined' || comment === null) {
+        console.log('Parameters are incorrect: email: ', email, ' comment: ', comment);
+        res.redirect('http://aixkare.com/thanks.html');
+    } else {
+        var ip = req.ip;
+        console.log('Req received from:',ip,' posting: ', req.body);
+        var currentDate = moment().format('YYYY/MM/DD');
+        var currentTime = moment().format('HH:mm:ss');
+        run(currentDate, currentTime, email, comment, ip).catch(console.log);
+        res.redirect('http://aixkare.com/thanks.html');
+    }
 });
 
 app.get('/contact_form', (req, res) => {
-    var email = req.query.email;
-    var comment = req.query.comment;
-    var ip = req.ip;
-    console.log('Req received from:', ip, ' getting: (', email,' ', comment, ')');
-    var currentDate = moment().format('YYYY/MM/DD');
-    var currentTime = moment().format('HH:mm:ss');
-    run(currentDate, currentTime, email, comment, ip).catch(console.log);
-    res.redirect('http://aixkare.com/thanks.html');
+    const email = req.query.email;
+    const comment = req.query.comment;
+    console.log('Req for comment received with: Email: <', email, '>, Comment: <', comment,'>');
+    if (typeof email === 'undefined' || email === null || typeof comment === 'undefined' || comment === null) {
+        console.log('Parameters are incorrect: email: ', email, ' comment: ', comment);
+        res.redirect('http://aixkare.com/thanks.html');
+    } else {
+        var ip = req.ip;
+        console.log('Req received from:',ip,' posting: ', req.body);
+        var currentDate = moment().format('YYYY/MM/DD');
+        var currentTime = moment().format('HH:mm:ss');
+        run(currentDate, currentTime, email, comment, ip).catch(console.log);
+        res.redirect('http://aixkare.com/thanks.html');
+    }
 });
 
 app.listen(port, function() {
